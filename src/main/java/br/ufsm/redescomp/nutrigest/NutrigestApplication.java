d66d778deb2e9f582309f2fbc50da8ca88d82b18
Érico <oliv.ericorib@gmail.com>
2024-10-21 13:39:13 -0300

    feat: iniciar repositório

---
+++ src/main/java/br/ufsm/redescomp/nutrigest/NutrigestApplication.java
@@ -0,0 +1,13 @@
+package br.ufsm.redescomp.nutrigest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrigestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrigestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufsm/redescomp/nutrigest/NutrigestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufsm.redescomp.nutrigest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrigestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
