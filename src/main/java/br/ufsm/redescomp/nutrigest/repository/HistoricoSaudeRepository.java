e108464bc661aeaa95ad93b7855dec13d008ddd9
Érico Ribeiro <oliv.ericorib@gmail.com>
2024-10-23 18:05:24 -0300

    fix: ajustar tipo de HistoricoSaudeRepository

--- src/main/java/br/ufsm/redescomp/nutrigest/repository/HistoricoSaudeRepository.java
+++ src/main/java/br/ufsm/redescomp/nutrigest/repository/HistoricoSaudeRepository.java
@@ -1,9 +1,9 @@
 package br.ufsm.redescomp.nutrigest.repository;
 
-import br.ufsm.redescomp.nutrigest.model.AtividadeFisica;
+import br.ufsm.redescomp.nutrigest.model.HistoricoSaude;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HistoricoSaudeRepository extends JpaRepository<HistoricoSaudeRepository, Long> {
+public interface HistoricoSaudeRepository extends JpaRepository<HistoricoSaude, Long> {
 }
