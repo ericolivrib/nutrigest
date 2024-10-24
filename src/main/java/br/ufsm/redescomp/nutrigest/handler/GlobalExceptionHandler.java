a9595cf545250a3fac8efc508ddf8530ed6b6332
Érico Ribeiro <oliv.ericorib@gmail.com>
2024-10-24 18:30:13 -0300

    feat: capturar exceções do tipo HttpMessageNotReadableException

---
+++ src/main/java/br/ufsm/redescomp/nutrigest/handler/GlobalExceptionHandler.java
@@ -0,0 +1,45 @@
+package br.ufsm.redescomp.nutrigest.handler;
+
+import br.ufsm.redescomp.nutrigest.dto.FieldErrorResponse;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<Void> handle404NotFoundError() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler({
+            MethodArgumentNotValidException.class,
+            HttpMessageNotReadableException.class
+    })
+    public ResponseEntity<List<FieldErrorResponse>> handle400BadRequestError(Exception e) {
+        List<FieldErrorResponse> errors = null;
+
+        if (e instanceof MethodArgumentNotValidException exception) {
+            errors = exception.getBindingResult().getFieldErrors()
+                    .stream()
+                    .map(f -> new FieldErrorResponse(f.getField(), Objects.requireNonNull(f.getDefaultMessage())))
+                    .toList();
+        } else if (e.getCause() instanceof MismatchedInputException exception) {
+            errors = List.of(new FieldErrorResponse(
+                    exception.getPath().getFirst().getFieldName(),
+                    "É esperado um valor de tipo " + exception.getTargetType().getSimpleName()
+            ));
+        }
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+}
